d4d37cbc784215e1b7f769c30f2df84990556e99
Bow <>
2024-03-24 17:14:31 -0700

    Added color support.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 
 /*
+ * THe default color to use when coloring output.
+ */
+const char* defaultRed = "31m";
+/*
+ * The ANSI escape sequence to reset the color (and everything else).
+ */
+const char* colorReset = "\e[0m";
+
+/*
  * Prints the help text then exits with exit code _status_.
  */
 void showHelp(int _status_){
@@ -12,9 +21,14 @@ void showHelp(int _status_){
  * The main function...
  */
 int main(int argc, char** argv){
+	//Options
+	char* color = const_cast<char*>(defaultRed);
+	bool showBytes = false, breakLines = true, colored;
+
+	//Check output destination
+	//TODO
+	
 	//Parse options
-	bool showBytes = false;
-	bool breakLines = true;
 	for(int i = 1; i < argc; ++i){
 		if(argv[i][0] != '-') showHelp(1);
 		switch(argv[i][1]){
@@ -27,6 +41,16 @@ int main(int argc, char** argv){
 			case 'h':
 				showHelp(0);
 				break; //Unneccessary
+			case 'c':
+				colored = true;
+				if(i+1 < argc){
+					if(argc && argv[i+1][0] == '-') continue;
+					color = const_cast<char*>(argv[++i]);
+				}
+				break;
+			case 'u':
+				colored = false;
+				break;
 			case '\0':
 				std::cout << ":3" << std::endl;
 				return 0;
@@ -43,29 +67,37 @@ int main(int argc, char** argv){
 		if(showBytes) std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)c << '\t';
 		switch(c){ //Escape non-printing characters.
 			case '\e':
-				std::cout << "\\e";
+				if(colored) std::cout << "\e[" << color << "\\e" << colorReset;
+				else std::cout << "\\e";
 				break;
 			case '\n':
-				std::cout << "\\n";
+				if(colored) std::cout << "\e[" << color << "\\n" << colorReset;
+				else std::cout << "\\n";
 				if(!showBytes && breakLines) std::cout << '\n';
 				break;
 			case '\a':
-				std::cout << "\\a";
+				if(colored) std::cout << "\e[" << color << "\\a" << colorReset;
+				else std::cout << "\\a";
 				break;
 			case '\b':
-				std::cout << "\\b";
+				if(colored) std::cout << "\e[" << color << "\\b" << colorReset;
+				else std::cout << "\\b";
 				break;
 			case '\f':
-				std::cout << "\\f";
+				if(colored) std::cout << "\e[" << color << "\\f" << colorReset;
+				else std::cout << "\\f";
 				break;
 			case '\r':
-				std::cout << "\\r";
+				if(colored) std::cout << "\e[" << color << "\\r" << colorReset;
+				else std::cout << "\\r";
 				break;
 			case '\t':
-				std::cout << "\\t";
+				if(colored) std::cout << "\e[" << color << "\\t" << colorReset;
+				else std::cout << "\\t";
 				break;
 			case '\v':
-				std::cout << "\\v";
+				if(colored) std::cout << "\e[" << color << "\\v" << colorReset;
+				else std::cout << "\\v";
 				break;
 			default: //Assume printing character and print.
 				std::cout << c;
