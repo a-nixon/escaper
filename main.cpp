d2c82324942a7cbe8fad21c00c0c2a01823acff0
Bow <>
2024-03-24 19:49:20 -0700

    Added basic help text.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iomanip>
 #include <iostream>
 
+#define NAME "escaper"
+
 /*
  * THe default color to use when coloring output.
  */
@@ -13,8 +15,15 @@ const char* colorReset = "\e[0m";
 /*
  * Prints the help text then exits with exit code _status_.
  */
-void showHelp(int _status_){
-	//TODO
+void showHelp(int _status_){ //Needs to be better.
+	std::cout << "Usage: " NAME " [flags]\n\n" NAME " is an output debugging tool. "
+			<< NAME " reads from stdin and escapes special characters before writing to stdout.\n\n"
+			<< "-h\tShow this help text.\n"
+			<< "-b\tShows the byte value (in hex) for each character.\n"
+			<< "-n\tDisables line breaks.\n"
+			<< "-c\tColors special characters. Optionally followed by a color sequence.\n"
+			<< "-u\tDisables colored output."
+			<< std::endl;
 	exit(_status_);
 }
 /*
@@ -52,7 +61,8 @@ int main(int argc, char** argv){
 				colored = false;
 				break;
 			case '\0':
-				std::cout << ":3" << std::endl;
+				std::cout << NAME " reads from stdin by default.\n\n";
+				showHelp(0);
 				return 0;
 			default:
 				showHelp(1);
@@ -66,36 +76,36 @@ int main(int argc, char** argv){
 	while((c = buffer->sbumpc()) != EOF){
 		if(showBytes) std::cout << std::setw(2) << std::setfill('0') << std::hex << (int)c << '\t';
 		switch(c){ //Escape non-printing characters.
-			case '\e':
+			case '\e': //ESC
 				if(colored) std::cout << "\e[" << color << "\\e" << colorReset;
 				else std::cout << "\\e";
 				break;
-			case '\n':
+			case '\n': //LF
 				if(colored) std::cout << "\e[" << color << "\\n" << colorReset;
 				else std::cout << "\\n";
 				if(!showBytes && breakLines) std::cout << '\n';
 				break;
-			case '\a':
+			case '\a': //BEL
 				if(colored) std::cout << "\e[" << color << "\\a" << colorReset;
 				else std::cout << "\\a";
 				break;
-			case '\b':
+			case '\b': //BS
 				if(colored) std::cout << "\e[" << color << "\\b" << colorReset;
 				else std::cout << "\\b";
 				break;
-			case '\f':
+			case '\f': //FF
 				if(colored) std::cout << "\e[" << color << "\\f" << colorReset;
 				else std::cout << "\\f";
 				break;
-			case '\r':
+			case '\r': //CR
 				if(colored) std::cout << "\e[" << color << "\\r" << colorReset;
 				else std::cout << "\\r";
 				break;
-			case '\t':
+			case '\t': //TAB
 				if(colored) std::cout << "\e[" << color << "\\t" << colorReset;
 				else std::cout << "\\t";
 				break;
-			case '\v':
+			case '\v': //VT
 				if(colored) std::cout << "\e[" << color << "\\v" << colorReset;
 				else std::cout << "\\v";
 				break;
